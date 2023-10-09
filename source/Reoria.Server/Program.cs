f99957760ca1faecec933e14d599e7c918f27207
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-10-08 22:41:31 -0400

    Rebuilding the game engine with a new roadmap.
    Opens #2
    Opens #3

--- source/Reoria.Server/Program.cs
+++ source/Reoria.Server/Program.cs
@@ -1,4 +1,1 @@
-﻿using Reoria.Server.Application;
-using Reoria.Server.Application.Services;
-
-using var server = new ServerApplication(args).Start<GameService>().Start<GameService>().Start<GameService>();
+﻿Console.WriteLine("Hello world!");
